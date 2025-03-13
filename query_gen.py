b43ce6a46e7543e31bfd4e6532717b17c4243140
Thomas Butler <thomas@nakamotomining.ai>
2025-03-13 10:48:32 -0500

    updates from cluster

--- query_gen.py
+++ query_gen.py
@@ -3,19 +3,45 @@ from bitagent.datasources import ToolDataset
 from neurons.validator import Validator
 from bitagent.protocol import QueryTask
 import random
-pass
 import json
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import argparse
 
-response_gen_model = "watt-ai/watt-tool-8B"
-tokenizer = AutoTokenizer.from_pretrained(response_gen_model)
+
+parser = argparse.ArgumentParser(description='Generate and evaluate tool call tasks')
+parser.add_argument('--model', type=str, default="BitAgent/BitAgent-8B",
+                    help='The model name or path to use for generation')
+parser.add_argument('--output', type=str, default="bitagent_output",
+                    help='Base name for output files (will be appended with _N.json)')
+args = parser.parse_args()
+
+response_gen_model = args.model
+output_base_name = args.output
+
+print(f"Using model: {response_gen_model}")
+print(f"Output files will be named: {output_base_name}_N.json")
+
+tokenizer = AutoTokenizer.from_pretrained(response_gen_model, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(response_gen_model, torch_dtype='auto', device_map='auto')
-system_prompt ="""You are an expert in composing functions. You are given a question, or just general problem statement, and a set of functions you can use. Based on the question, you will need to make one or more function/tool calls to achieve the purpose.
-You should only return the function call in tools call sections.
-You MUST put the function call in the format of func_name1(params_name1=params_value1, params_name2=params_value2...)
+
+system_prompt ="""
+You are an expert in composing functions.
+You are given a user query and a set of possible functions.
+Based on the query, you will need to make one or more function/tool calls to achieve the purpose.
+The query may not be in the form of a question. 
+
+ONLY return a function call.
+If none of the tools work, STILL RETURN A FUNCTION CALL. 
+you MUST put it in the format of [func_name(params_name1=params_value1, params_name2=params_value2...)].
 You SHOULD NOT include any other text in the response.
-MAKE SURE to use the correct function names, and double check the values passed to the function call for logic errors.
-Here is a list of functions in JSON format that you can invoke.\n{functions}\n
+
+Available tools (python format) below:
+
+----  TOOLS ----
+{tools}
+----------------
+
+Function call:
 """
 
 TASK_FREQUENCY = {
@@ -47,9 +73,8 @@ task_rewards = []
 
 tasks_and_rewards = []
 
-# 5 sets of 10000 records
 for i in range(5):
-    for _ in range(10000):
+    for _ in range(1000):
         try:
             match choice:
                 case "tool_call":
@@ -61,32 +86,36 @@ for i in range(5):
 
                     user_query = tool_call_task.messages[0].content
                     tools = tool_call_task.synapse.tools
-
-                    messages = [
-                        {'role': 'system', 'content': system_prompt.format(functions=tools)},
-                        {'role': 'user', 'content': user_query}
+                    
+                    input = [
+                        {"role": "system", "content": system_prompt.format(tools=tools)},
+                        {"role": "user", "content": user_query}
                     ]
-                    inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(model.device)
+                    print(f"input:\n{input}")
+                    
+                    inputs = tokenizer.apply_chat_template(input, return_tensors="pt").to(model.device)
 
                     outputs = model.generate(inputs, max_new_tokens=512, do_sample=False, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
                     output = tokenizer.decode(outputs[0][len(inputs[0]):], skip_special_tokens=True)
                     syn.response = output
+                    print(f"response:\n{output}")
 
                     # [total_score, total_possible, results, correct_answer]
                     task_reward = tool_call_task.reward(validator=val, synapse=syn)
                     task_rewards.append(task_reward)
-                    print(f"scored response: {task_reward[0]}/{task_reward[1]}")      
+                    print(f"scored response: {task_reward[0]}/{task_reward[1]}")
                     tasks_and_rewards.append({"task": tool_call_task, "response": syn.response, "reward": task_reward})
 
         except Exception as e:
             #bt.logging.warning(f'Error getting task (name {choice}): ', e)
             #bt.logging.warning(traceback.format_exc())
+            raise e
             print(f'Error getting task (name {choice}): ', e)
 
 
 
     # Write tasks_and_rewards to json file
-    output_file = f"tasks_and_rewards_{i}.json"
+    output_file = f"output/{output_base_name}/{output_base_name}_{i}.json"
     serializable_data = []
 
     for item in tasks_and_rewards:
