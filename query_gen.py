7259e481798eb600f805c24899257cf3e147c121
Thomas Robert Butler IV <thomas@nakamotomining.ai>
2025-03-16 12:35:09 -0500

    update system prompt

--- query_gen.py
+++ query_gen.py
@@ -38,25 +38,19 @@ model = AutoModelForCausalLM.from_pretrained(
     response_gen_model, torch_dtype="auto", device_map="auto"
 )
 
-system_prompt = """
-You are an expert in composing functions.
-You are given a user query and a set of possible functions.
-Based on the query, you will need to make one or more function/tool calls to achieve the purpose.
-The query may not be in the form of a question.
+system_prompt = """You are an expert in composing functions.
+You are given a question and a set of possible functions.
+Based on the question, you will need to make one or more function/tool calls to achieve the purpose.
+If none of the functions can be used, point it out.
+If the given question lacks the parameters required by any function, also point it out.
+You should only return the function call in tools call sections.
 
-ONLY return a function call.
-If none of the tools work, STILL RETURN A FUNCTION CALL.
+If you decide to invoke any of the function(s),
 you MUST put it in the format of [func_name(params_name1=params_value1, params_name2=params_value2...)].
 You SHOULD NOT include any other text in the response.
+Here is a list of functions in JSON format that you can invoke:
 
-Available tools (python format) below:
-
-----  TOOLS ----
-{tools}
-----------------
-
-Function call:
-"""
+{functions}"""
 
 TASK_FREQUENCY = {
     "tool_call": 1,
@@ -173,7 +167,7 @@ while True:
                             outputs[0][len(inputs[0]) :], skip_special_tokens=True
                         )
                     syn.response = output
-                    print(f"response:\n{output}")
+                    print(f"response: {output}")
 
                     # [total_score, total_possible, results, correct_answer]
                     task_reward = tool_call_task.reward(validator=val, synapse=syn)
